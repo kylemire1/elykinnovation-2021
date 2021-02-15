c2684f680fee45fe18488218efedd262e12f0c61
Kyle Lemire <kyle@elykinnovation.com>
2021-02-15 10:02:32 -0600

    fix: reverting

--- static/functions/create-submission.js
+++ static/functions/create-submission.js
@@ -1,7 +1,7 @@
 // This is a Netlify function. There is more configuration involved than just this file to get this working!
 // See the Gatsby docs here for how to set it up: https://support.gatsbyjs.com/hc/en-us/articles/360054529274-Deploying-Netlify-Functions-from-Gatsby-Cloud
 // To learn how to set things up on the WordPress side, watch this video: https://www.youtube.com/watch?v=ZRQ94PMNEcg
-const fetch = request('node-fetch').default;
+const fetch = require('node-fetch').default;
 
 exports.handler = async function (event) {
   const headers = {
