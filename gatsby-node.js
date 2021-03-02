3a83c8477cafe20096ad4b0434deb86f89abcb7f
Kyle Lemire <hello@klemire.com>
2021-03-02 13:39:01 -0600

    tweaking build method

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,5 @@
 const path = require(`path`)
 const chunk = require(`lodash/chunk`)
-const util = require('util')
-const child_process = require('child_process')
-const exec = util.promisify(child_process.exec)
 
 // This is a simple debugging tool
 // dd() will prettily dump to the terminal and kill the process
@@ -295,24 +292,4 @@ async function getPages({ graphql, reporter }) {
   }
 
   return graphqlResult.data.allWpPage.edges
-}
-
-// Under the hood, Gatsby Cloud uses @netlify/zip-it-and-ship-it to deploy your functions,
-// which does not build, transpile, or install the function dependencies; this needs to be done
-// before deployment.
-exports.onPostBuild = async gatsbyNodeHelpers => {
-  const { reporter } = gatsbyNodeHelpers
-
-  const reportOut = report => {
-    const { stderr, stdout } = report
-    if (stderr) reporter.error(stderr)
-    if (stdout) reporter.info(stdout)
-  }
-
-  // NOTE: the gatsby build process automatically copies /static/functions to /public/functions
-  // The exec() functions executes command line scripts. Here we're using it to build the functions
-  // If you use yarn, replace "npm install" with "yarn install"
-  reportOut(
-    await exec('cd ./public/functions && npm install --cache /tmp/empty-cache')
-  )
 }
